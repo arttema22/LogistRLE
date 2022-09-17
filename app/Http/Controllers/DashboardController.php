093a3de9950e71f3313e22bc686802392285eb61
Artem Gusev <arttema@mail.ru>
2022-09-17 14:34:31 +0300

    Edit main page

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -11,30 +11,28 @@ use App\Models\Refilling;
 use App\Models\Routes;
 use App\Models\Services;
 use App\Models\Salary;
-use Maatwebsite\Excel\Facades\Excel;
-use App\Export\ProfitExport;
 use Illuminate\Support\Facades\Gate;
-use App\Http\Filters\ProfitFilter;
-use PhpOffice\PhpSpreadsheet\IOFactory;
-
-
-use alhimik1986\PhpExcelTemplator\PhpExcelTemplator;
-use alhimik1986\PhpExcelTemplator\params\ExcelParam;
-use alhimik1986\PhpExcelTemplator\params\CallbackParam;
-use alhimik1986\PhpExcelTemplator\setters\CellSetterArrayValueSpecial;
-use Illuminate\Support\Carbon;
 
 class DashboardController extends Controller
 {
-    // список сверок
+
     public function index(Request $request)
     {
-        $RoutesAll = Routes::count();
-        $RoutesActive = Routes::where('status', 1)->count();
-        $RefillingsAll = Refilling::count();
-        $RefillingsActive = Refilling::where('status', 1)->count();
-        $SalaryAll = Salary::count();
-        $SalaryActive = Salary::where('status', 1)->count();
+        if (Gate::allows('is-driver')) {
+            $RoutesAll = Routes::where('driver_id', Auth::user()->id)->count();
+            $RoutesActive = Routes::where('driver_id', Auth::user()->id)->where('status', 1)->count();
+            $RefillingsAll = Refilling::where('driver_id', Auth::user()->id)->count();
+            $RefillingsActive = Refilling::where('driver_id', Auth::user()->id)->where('status', 1)->count();
+            $SalaryAll = Salary::where('driver_id', Auth::user()->id)->count();
+            $SalaryActive = Salary::where('driver_id', Auth::user()->id)->where('status', 1)->count();
+        } else {
+            $RoutesAll = Routes::count();
+            $RoutesActive = Routes::where('status', 1)->count();
+            $RefillingsAll = Refilling::count();
+            $RefillingsActive = Refilling::where('status', 1)->count();
+            $SalaryAll = Salary::count();
+            $SalaryActive = Salary::where('status', 1)->count();
+        }
         return view('home', [
             'RoutesAll' => $RoutesAll, 'RoutesActive' => $RoutesActive,
             'RefillingsAll' => $RefillingsAll, 'RefillingsActive' => $RefillingsActive,
